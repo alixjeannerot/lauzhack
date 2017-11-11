8160551a8b602263248b99a13eaf2c3b4f14e17b
Xavier Pantet <xavpantet@gmail.com>
2017-11-11 18:00:40 +0100

    Server can now serve static files such as .js and images

--- src/ch/epfl/lauzhack/skyscanner/main/index.ts
+++ src/ch/epfl/lauzhack/skyscanner/main/index.ts
@@ -2,6 +2,8 @@ const express = require('express');
 import {Request, Response} from "express"
 const app = express();
 
+app.use(express.static('build'));
+
 app.get('/', function(re: Request, res: Response) {
     res.setHeader('Content-Type', 'text/html');
     res.render('index.ejs', {});
