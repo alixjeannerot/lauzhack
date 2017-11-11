9977d4f79d47e0551be1fea6e89a95a5846c88c4
Xavier Pantet <xavpantet@gmail.com>
2017-11-11 16:35:28 +0100

    Updated main server

--- src/ch/epfl/lauzhack/skyscanner/main/index.ts
+++ src/ch/epfl/lauzhack/skyscanner/main/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello World");
+import http = require('http');
+import os = require('os');
+import fs = require('fs');
+
+class HttpServer {
+    nodePort: number;
+
+    constructor (port: number) {
+        this.nodePort = port;
+    }
+
+    private static onRequest(request: http.IncomingMessage, response: http.ServerResponse) {
+        response.writeHead(200);
+        response.write("Hello World");
+        response.end();
+    }
+
+    onStart() {
+        let httpServer = http.createServer(HttpServer.onRequest);
+        httpServer.listen(this.nodePort);
+        console.log('Server listening on http://' + os.hostname() + ':' + this.nodePort + '/');
+    }
+}
+
+new HttpServer(8080).onStart();
