65c39c6d2e65b3103329bf1c80136941aa878ca1
Xavier Pantet <xavpantet@gmail.com>
2017-11-11 22:01:57 +0100

    First example of session utilization

--- src/ch/epfl/lauzhack/skyscanner/main/index.ts
+++ src/ch/epfl/lauzhack/skyscanner/main/index.ts
@@ -1,16 +1,24 @@
 const express = require('express');
-import {Request, Response} from "express"
+const session = require('express-session');
+
 const app = express();
 
+app.use(session({secret: 'skyscanner2luxe', resave: false, saveUninitialized: false, cookie: { maxAge: 60000 }}));
 app.use(express.static('build'));
 app.use(express.static('static'));
 
-app.get('/', function(re: Request, res: Response) {
+app.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.render('index.ejs', {});
+    if(req.session.user) {
+        res.render('index.ejs', {user: req.session.user});
+    }
+    else{
+        req.session.user = "Hello";
+        res.render('index.ejs', {user: "nobody"});
+    }
 });
 
-app.use(function(req: Request, res: Response){
+app.use(function(req, res){
 
 });
 
