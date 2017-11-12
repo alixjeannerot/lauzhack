42a7ef3f5ace926e6dbf07f9d920629653c5ab5a
Alix Jeannerot <alix.jeannerot@epfl.ch>
2017-11-12 05:03:17 +0100

    init

--- static/js/app.ts
+++ static/js/app.ts
@@ -2,15 +2,18 @@ import SkyscannerRequest from "../../src/ch/epfl/lauzhack/skyscanner/request/Sky
 import User from "../../src/ch/epfl/lauzhack/skyscanner/game/objects/User";
 
     export default class App{  
+    private toDisplay : Array<Jouney>;
     private static req: SkyscannerRequest = new SkyscannerRequest();    
     public static run(user: User): void{  
         console.log(user); 
         App.updateMap();  
+        toDisplay=Array();
     }    
+
     private static updateMap(): void{  
-        /*req.withDestPlace  
+       /* req.withDestPlace  
         req.withOriginPlace  
         req.withCurrCOuntr (chainable)    
-        getFlightQuote.then((fr: FlightResponse) => { fr.toJourney() }).catch(error => {});   */
+        getFlightQuote.then((fr: FlightResponse) => { fr.toJourney() }).catch(error => {}).to;   */
     }
 }
